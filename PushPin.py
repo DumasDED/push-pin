31d3e5295018cdb724ab87cdd74fcb14a2fd9bb2
DDumas <DDumas@enstoa.com>
2017-08-17 00:57:30 -0400

    search function

--- PushPin.py
+++ PushPin.py
@@ -1,50 +1,46 @@
 import json
 import itertools
 
+
+def name_match(pl, st, precision):
+    if len(st) == 2:
+        return pl['admincode'] == st                                        # Two-letter strings == state abbrs.
+    elif precision == 0:
+        return st in pl['name'] or st in pl['alternatenames']               # String in name or alternate names
+    elif precision == 1:
+        return st == pl['name'] or st in pl['alternatenames']               # String matches name or in alternate names
+    else:
+        return st == pl['name']                                             # String matches name
+
+
 # TODO: split data out between states and non-states
-with open('json/locations.json') as f:
+with open('json/locations.json') as f:                                      # Load from JSON
     places = json.load(f)
 
-search = ['Garwood', 'NJ']
+search = ['Crown Heights']                                                  # Search string (split)
 
 states = [place for place in places if place['featurecode'] == 'ADM1']      # All states
 
 for string in search:
-
-    # TODO: define function that encapsulates if logic for different search strings
-    codes = [place['admincode']
-             for place in places
-             if place['name'] == string
-             or string in place['alternatenames']]
-
+    codes = [place['admincode'] for place in places if name_match(place, string, 1)]
     codes = list(set(codes))
-
     states = [state for state in states for code in codes if state['admincode'] in code]
 
 states = [k for k, v in itertools.groupby(states)]                          # Remove duplicate references
 
-cities = [place                                                             # All cities in filtered states
-          for place in places
-          for state in states
+cities = [place for place in places for state in states                     # All cities in filtered states
           if place['featureclass'] == 'P'
           and state['admincode'] in place['admincode']]
 
-cities = [k for k, v in itertools.groupby(cities)]
+cities = [k for k, v in itertools.groupby(cities)]                          # Remove duplicate references
 
 city_candidates = []
 
 for string in search:
-
-    city_candidates.extend([city for city in cities
-                            if city['name'] == string
-                            or string in city['alternatenames']])
+    city_candidates.extend([city for city in cities if name_match(city, string, 1)])
 
 for city in city_candidates:
     print city['name'], city['admincode'], city['featurecode']
 
-
-"""
-Cities will be a different strategy. I think I will need to do an additive process rather than
-a reductive one, adding all the matches for each string in the list and then determining the
-highest-ranking populated place among those included.
-"""
+# TODO: incorporate enum of prioritized PPL feature codes to allow selection of highest ranking PPL
+# TODO: add population to place data; select highest populated area if all else fails
