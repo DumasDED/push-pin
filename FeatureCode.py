23eafde2cd804504e0e92afd7ef4e8b3e8802540
DDumas <DDumas@enstoa.com>
2017-08-24 23:36:53 -0400

    Working script

---
+++ FeatureCode.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class FeatureCode(Enum):
+    ADM1 = 1
+    ADM2 = 2
+    ADM3 = 3
+    ADMD = 4
+    PPLC = 5
+    PPLG = 6
+    PPLA = 7
+    PPLA2 = 8
+    PPLA3 = 9
+    PPLA4 = 10
+    PPL = 11
+    PPLX = 12
+    PPLS = 13
+    PPLL = 14
--- PushPin.py
+++ PushPin.py
@@ -1,46 +1,77 @@
+import string
 import json
 import itertools
 
+from collections import Counter
+from datetime import datetime
+from FeatureCode import FeatureCode
 
-def name_match(pl, st, precision):
-    if len(st) == 2:
-        return pl['admincode'] == st                                        # Two-letter strings == state abbrs.
-    elif precision == 0:
-        return st in pl['name'] or st in pl['alternatenames']               # String in name or alternate names
-    elif precision == 1:
-        return st == pl['name'] or st in pl['alternatenames']               # String matches name or in alternate names
-    else:
-        return st == pl['name']                                             # String matches name
-
+start = datetime.now()
 
 # TODO: split data out between states and non-states
-with open('json/locations.json') as f:                                      # Load from JSON
+with open('json/states.json') as f:
+    states = json.load(f)
+
+with open('json/nonstates.json') as f:
     places = json.load(f)
 
-search = ['Crown Heights']                                                  # Search string (split)
+print datetime.now() - start, ": Loading data"
 
-states = [place for place in places if place['featurecode'] == 'ADM1']      # All states
+search = ['Garwood']                                           # Search string (split)
+
+print datetime.now() - start, ": Creating states list"
 
 for string in search:
-    codes = [place['admincode'] for place in places if name_match(place, string, 1)]
-    codes = list(set(codes))
-    states = [state for state in states for code in codes if state['admincode'] in code]
+    state_candidates = [
+        state for state in states
+        if string == state['admincode']
+        or string == state['name']
+        or string in state['alternatenames']
+    ]
+    if len(state_candidates) > 0:
+        search.pop(search.index(string))
+        states = state_candidates
+        break
 
-states = [k for k, v in itertools.groupby(states)]                          # Remove duplicate references
+print datetime.now() - start, ": Filtering states list by search criteria"
 
-cities = [place for place in places for state in states                     # All cities in filtered states
-          if place['featureclass'] == 'P'
-          and state['admincode'] in place['admincode']]
+if len(states) == 51:
+    cities = filter(lambda x: x['featureclass'] == 'P', places)
+else:
+    cities = filter(lambda x: x['featureclass'] == 'P'
+                    and any([s for s in states if s['admincode'] in x['admincode']])
+                    , places)
 
-cities = [k for k, v in itertools.groupby(cities)]                          # Remove duplicate references
+print datetime.now() - start, ": Creating cities list from filtered states list"
 
 city_candidates = []
 
 for string in search:
-    city_candidates.extend([city for city in cities if name_match(city, string, 1)])
+    city_candidates.extend([
+        city for city in cities
+        if string == city['name']
+        or string in city['alternatenames']
+         ])
+
+print datetime.now() - start, ": Filtering cities list for search string"
+
+codes = Counter([znk['admincode'] for znk in city_candidates])
+maxcount = max([codes[i] for i in codes.keys()])
+maxcodes = [x for x in codes.keys() if codes[x] == maxcount]
+
+print datetime.now() - start, ": Retrieving most frequently occurring admin code"
+
+city_candidates = filter(lambda x: x['admincode'] in maxcodes, cities)
+
+print datetime.now() - start, ": Filtering city candidates based on most frequently occurring admin code(s)"
+
+city_level = min(map(lambda x: FeatureCode[x['featurecode']].value, city_candidates))
 
 for city in city_candidates:
     print city['name'], city['admincode'], city['featurecode']
 
-# TODO: incorporate enum of prioritized PPL feature codes to allow selection of highest ranking PPL
+city_candidates = [city for city in city_candidates if city['featurecode'] == FeatureCode(city_level).name]
+
+print datetime.now() - start, ": Retrieving highest administrative division(s)"
+
 # TODO: add population to place data; select highest populated area if all else fails
