0e1cbac5f8b3478a884a7326cb71d7f341ca9036
DDumas <DDumas@enstoa.com>
2017-08-26 07:37:29 -0400

    Additional tweaks

--- PushPin.py
+++ PushPin.py
@@ -4,6 +4,7 @@ import itertools
 
 from collections import Counter
 from datetime import datetime
+from fuzzywuzzy import fuzz
 from FeatureCode import FeatureCode
 
 start = datetime.now()
@@ -17,21 +18,25 @@ with open('json/nonstates.json') as f:
 
 print datetime.now() - start, ": Loading data"
 
-search = ['Garwood']                                           # Search string (split)
+search = ['Crown Heights']                                           # Search string (split)
 
 print datetime.now() - start, ": Creating states list"
 
+state_candidates = []
+
 for string in search:
-    state_candidates = [
+    x = [
         state for state in states
         if string == state['admincode']
         or string == state['name']
         or string in state['alternatenames']
     ]
-    if len(state_candidates) > 0:
+    if len(x) > 0:
         search.pop(search.index(string))
-        states = state_candidates
-        break
+        state_candidates.extend(x)
+
+if state_candidates != []:
+    states = state_candidates
 
 print datetime.now() - start, ": Filtering states list by search criteria"
 
@@ -55,23 +60,29 @@ for string in search:
 
 print datetime.now() - start, ": Filtering cities list for search string"
 
+for city in city_candidates:
+    print '\t', city['name'], city['admincode'], city['featurecode']
+
 codes = Counter([znk['admincode'] for znk in city_candidates])
 maxcount = max([codes[i] for i in codes.keys()])
 maxcodes = [x for x in codes.keys() if codes[x] == maxcount]
 
-print datetime.now() - start, ": Retrieving most frequently occurring admin code"
+print datetime.now() - start, ": Retrieving most frequently occurring admin code(s)"
 
-city_candidates = filter(lambda x: x['admincode'] in maxcodes, cities)
+city_candidates = filter(lambda x: x['admincode'] in maxcodes, city_candidates)
 
 print datetime.now() - start, ": Filtering city candidates based on most frequently occurring admin code(s)"
 
-city_level = min(map(lambda x: FeatureCode[x['featurecode']].value, city_candidates))
-
 for city in city_candidates:
-    print city['name'], city['admincode'], city['featurecode']
+    print '\t', city['name'], city['admincode'], city['featurecode']
+
+city_level = min(map(lambda x: FeatureCode[x['featurecode']].value, city_candidates))
 
 city_candidates = [city for city in city_candidates if city['featurecode'] == FeatureCode(city_level).name]
 
 print datetime.now() - start, ": Retrieving highest administrative division(s)"
+
+for city in city_candidates:
+    print '\t', city['name'], city['admincode'], city['featurecode']
 
 # TODO: add population to place data; select highest populated area if all else fails
